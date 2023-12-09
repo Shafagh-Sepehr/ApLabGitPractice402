78f72a96306578e7b2105e07e80072f6f21b25ab
ansarielmira <ansarielmira692@gmail.com>
2023-12-09 19:19:36 +0330

    Add existing file

--- 1.cpp
+++ 1.cpp
@@ -6,8 +6,9 @@ class container {
 	int size;
 public:
 	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container(){}
+	container(int s) :size(s) {}
+	 int& getsize() { return size; }
 
 };
 
@@ -15,12 +16,12 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	 vector(int l) :len(l){
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen()  {
+		call_num++;
 		return len;
 	}
 	~vector() = default;
@@ -28,13 +29,19 @@ public:
 
 int main() {
 
-	container c1(100);
-	vector v1 = c1;
+	//container c1(100);
+	//vector v1 = c1;
+	// 
+	vector v1(1);
+	container c1= v1;
+	
+	//container c1(100)=v1;
+	
 	container& r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
 	vector v2 = 100;
-	v2.getlen = 40;
+	//v2.getlen = 40;
 	cout << v2.getlen();
 }
