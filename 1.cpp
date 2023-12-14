366d786db0d05e480a56a8a5ccab41d9395803e9
amirsoleimani7 <amirsoleimani3131@gmail.com>
2023-12-14 18:40:22 +0330

    bugs fixed in 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -7,19 +7,18 @@ class container {
 public:
 	float* p;
 	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	int& getsize() { return size;}//removing size so it can be used both as getter and as setter
 
 };
-
 class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l),container(1 * 100){ //should use cunstrcutor
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
+	int& getlen() { //removing const becuase it's changing a value in the body
 		call_num ++;
 		return len;
 	}
@@ -29,12 +28,12 @@ public:
 int main() {
 
 	container c1(100);
-	vector v1 = c1;
-	container& r1 = v1;
+	//vector v1 = c1;
+	//container& r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2(100); //put it in () becuase the custrcutor is explicit
+	v2.getlen() = 40;//it's a function so getlen() is correct
 	cout << v2.getlen();
 }
